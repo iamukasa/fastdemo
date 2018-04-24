701dbda39a4954cfd846fc79b54786919ee24bf5
Irving Amukasa <amukach@gmail.com>
2018-04-24 14:15:56 +0300

    Delete credentials.py

--- credentials.py
+++
@@ -1,5 +0,0 @@
-consumer_key = 'efhWX20yH2mZwrScEVhGWazcs'
-consumer_secret ='5fpfbs7EeXzdRRHGZDpv5MX307LpPIto8JPV9LFmZwaR0DqIMA'
-access_token = '3538319836-QYhOvZs0bpAIVVfrEDX2YJ84A2DVTgxrrAJnoOr'
-access_token_secret ='chEMydMylmcLck5ltwf1fYiqY0Sv7itD2wSV3t2ocbJPw'
-
